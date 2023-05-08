49ae2274a19ce929139026e4591b26f107c66b6e
Alice Chen <alice@opencontext.com>
2023-05-08 13:36:53 -0700

    Initial commit

---
+++ src/main/java/com/scatterly/crates/App.java
@@ -0,0 +1,13 @@
+package com.scatterly.crates;
+
+/**
+ * Crates ERP!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Crates ERP!" );
+    }
+}
